2209fb3d8a965820aade5f351931d0719bf4cfc7
Megan's ISU <megansISU@eds-air-2.student.iastate.edu>
2017-09-10 15:53:55 -0500

    Baseball Player class

--- src/BaseballPlayer.java
+++ src/BaseballPlayer.java
@@ -2,4 +2,13 @@
  * Created by megansISU on 9/10/17.
  */
 public class BaseballPlayer {
+    private String name;
+    private int gamesPlayed;
+
+    public BaseballPlayer(String name, int games){
+        this.name = name;
+        gamesPlayed = games;
+    }
+
 }
+
