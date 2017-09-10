e9512910bdbefc324ddaa1fc0e6dd5bfe85274bc
Megan's ISU <megansISU@eds-air-2.student.iastate.edu>
2017-09-10 16:03:14 -0500

    Fielder class

--- src/Fielder.java
+++ src/Fielder.java
@@ -2,4 +2,19 @@
  * Created by megansISU on 9/10/17.
  */
 public class Fielder {
+    int assists;
+    int putouts;
+    int errors;
+    double fieldingPercentage;
+
+    public Fielder(int assists, int putouts, int errors){
+        this.assists = assists;
+        this.putouts = putouts;
+        this.errors = errors;
+        setFieldingPercentage();
+    }
+
+    private void setFieldingPercentage(){
+        fieldingPercentage = (double)(assists + putouts) / (assists + putouts + errors);
+    }
 }
