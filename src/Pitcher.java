552a9955067f15275062d67e99378eae578b4564
Megan's ISU <megansISU@eds-air-2.student.iastate.edu>
2017-09-10 16:00:45 -0500

    Pitcher class

--- src/Pitcher.java
+++ src/Pitcher.java
@@ -2,4 +2,17 @@
  * Created by megansISU on 9/10/17.
  */
 public class Pitcher {
+    int inningsPitched;
+    int earnedRuns;
+    double era;
+
+    public Pitcher(int inningsPitched, int earnedRuns){
+        this.inningsPitched = inningsPitched;
+        this.earnedRuns = earnedRuns;
+        setERA();
+    }
+
+    private void setERA(){
+        era = (9.0 * earnedRuns) / inningsPitched;
+    }
 }
