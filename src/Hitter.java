b0776895dc7610ae8e4fb51bceb301c3f9effd8d
Megan's ISU <megansISU@eds-air-2.student.iastate.edu>
2017-09-10 15:55:55 -0500

    Hitter class

--- src/Hitter.java
+++ src/Hitter.java
@@ -2,4 +2,17 @@
  * Created by megansISU on 9/10/17.
  */
 public class Hitter {
+    int atBat;
+    int hits;
+    double battingAve;
+
+    public Hitter (int atBat, int hits){
+        this.atBat = atBat;
+        this.hits = hits;
+        setBattingAve();
+    }
+
+    private void setBattingAve(){
+        battingAve = (double) hits / atBat;
+    }
 }
